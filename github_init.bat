2d94c34114881000f5b0dd66533c5909cd0dfc31
duyluc <duongduyluc3@gmail.com>
2022-04-21 09:32:18 +0700

    Update file

---
+++ github_init.bat
@@ -0,0 +1,7 @@
+echo "# ITG3205" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/duyluc/ITG3205.git
+git push -u origin master
